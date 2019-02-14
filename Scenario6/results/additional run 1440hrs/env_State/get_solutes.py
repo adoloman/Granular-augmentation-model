3a784705e3e9cf1f1fa268559764b55d227e6a21
adoloman <anna.doloman@gmail.com>
2019-02-14 09:38:57 -0700

    results from scenario 6 and 7 simulations

---
+++ Scenario6/results/additional run 1440hrs/env_State/get_solutes.py
@@ -0,0 +1,27 @@
+import os, sys
+import xml.etree.ElementTree as ET
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+filename = r'env_State(1440).xml'
+
+pixel_volume = 64 * pow (10, -15)
+columns = 127
+
+if __name__ == "__main__":
+	tree = ET.parse(os.path.join (BASE_DIR, filename))
+	root = tree.getroot()
+
+	for solute in root.iter('solute'):
+		name = solute.attrib['name']
+		# need to split by lines, but it generates array fo strings with ";" in the end
+		# also, sometimes '' values occures
+		data = solute.text.splitlines()
+		# get rid of ";" and ''
+		data = [ float (item.strip(';')) for item in data if item != '']
+		tmp = sum(data)
+		print ('{name} :\n\tMass :\t{biomass} \n\tAverage concentration :\t{concentration}'.format(
+			name=name,
+			biomass = tmp * pixel_volume,
+			concentration = tmp / columns**2
+			))
